07e354f5f5e6337dd2dc16391ad68a1a143f5e4d
Rasel9360 <raselmahamud9360@gmail.com>
2024-04-07 21:42:03 +0600

    project setup & navbar added

---
+++ src/Root/Root.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Pages/Navbar/Navbar";
+const Root = () => {
+    return (
+        <div >
+            <Navbar></Navbar>
+            <div className="w-11/12 mx-auto">
+                <Outlet></Outlet>
+            </div>
+        </div>
+    );
+};
+
+export default Root;
