466ca8825872af6158ae7b8172ea4c9469b60964
Rasel9360 <raselmahamud9360@gmail.com>
2024-04-17 19:18:08 +0600

    update firebase config file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCOZ3nFunWmJehhapOUJG14n3laqTUPHuQ",
-  authDomain: "nr-holding.firebaseapp.com",
-  projectId: "nr-holding",
-  storageBucket: "nr-holding.appspot.com",
-  messagingSenderId: "1087965004034",
-  appId: "1:1087965004034:web:0b3cec2a80697ed4bd421b"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
